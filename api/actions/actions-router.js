900f71c9b85917de2c9c8fdf92f432119771cb48
Austin Carman <abcarman12@gmail.com>
2021-06-04 11:58:07 -0600

    wip

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const Actions = require('./actions-model');
 const {
-    validateActionsId
+    validateActionsId,
+    validateAction,
 } = require('../middleware/middlewareActions');
 
 const router = express.Router()
@@ -14,8 +15,22 @@ router.get('/', (req, res, next) => {
         .catch(next)
 })
 
-router.get('/', validateActionsId, (req, res, next) => {
-    
+router.get('/:id', validateActionsId, (req, res, next) => {
+    res.status(200).json(req.action);
 })
+
+router.post('/', validateAction, (req, res, next) => {
+    Actions.insert(req.action)
+        .then(newAction => {
+            res.status(201).json(newAction)
+        })
+        .catch(next)
+})
+
+router.put('/:id', validateActionsId, validateAction, (req, res, next) => {
+
+})
+
+
 
 module.exports = router;
--- api/middleware/middlewareActions.js
+++ api/middleware/middlewareActions.js
@@ -1,10 +1,37 @@
 const Actions = require('../actions/actions-model');
 
 function validateActionsId(req, res, next) {
-    
-    next()
+    Actions.get(req.params.id)
+        .then(action => {
+            if(!action) {
+                res.status(404).json({
+                    message: `action with id ${req.params.id} not found`
+                })
+            } else {
+                req.action = action
+                next()
+            }
+        })
+        .catch(next)
+}
+
+function validateAction(req, res, next) {
+    const { project_id, description, notes } = req.body
+    if(!project_id || !description || !notes) {
+        res.status(400).json({
+            message: 'must provide project_id, description, and notes'
+        })
+    } else {
+        req.action = {
+            project_id: req.body.project_id,
+            description: req.body.description,
+            notes: req.body.notes
+        }
+        next()
+    }
 }
 
 module.exports = {
-    validateActionsId
+    validateActionsId,
+    validateAction,
 }
